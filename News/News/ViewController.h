1e0d70a382b8a11fef0b162b551d65a50364cc04
sunjinshuai <s_mike@163.com>
2015-07-31 13:16:25 +0800

    初始化项目

---
+++ News/News/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  News
+//
+//  Created by 孙金帅 on 15/7/31.
+//  Copyright (c) 2015年 heima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ News/News/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  News
+//
+//  Created by 孙金帅 on 15/7/31.
+//  Copyright (c) 2015年 heima. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
